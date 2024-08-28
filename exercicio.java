022d53f39b47bffae09266697ff78da6d785140e
e.mendes <162847853+emendes19@users.noreply.github.com>
2024-08-28 13:12:54 +0000

    add: 28-08-24

---
+++ exercicio.java
@@ -0,0 +1,16 @@
+class exercicio{
+    public static void main(String[] args){
+    int janeiro;
+    janeiro = 1500;
+    int fevereiro;
+    fevereiro = 2300;
+    int março; 
+    março = 1700;
+    int T;
+    T = janeiro + fevereiro + março;
+    System.out.println("janeiro: " + janeiro);
+    System.out.println("fevereiro: " + fevereiro);
+    System.out.println("março: " + março);
+    System.out.println("Total: " + T);
+    }
+}
---
+++ idade.java
@@ -0,0 +1,23 @@
+class Idade{
+    public static void main(String[] args){
+        int idade;
+        idade = 15;
+        int IdadeAnoQueVem = idade + 1;
+        double metadeidade = idade / 2.0;
+        boolean maioridade = idade >= 18;
+        boolean TipoSangue;
+        TipoSangue = 'a' == 'A';
+
+        int i = 5;
+        int j = i;
+        i = i + 1;
+
+        System.out.println(idade);
+        System.out.println(IdadeAnoQueVem);
+        System.out.println(metadeidade);
+        System.out.println(maioridade);
+        System.out.println(TipoSangue);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
